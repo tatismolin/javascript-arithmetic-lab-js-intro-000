eb7b32357333d79f5b9ebacfdf77a3f160bfa868
immutable-illustrator-3998 <tatismolin@gmail.com>
2019-08-17 03:30:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+  return parseInt(a) + parseInt(b)
+}
+
+function subtract(b, a) {
+  return parseInt(b) - parseInt(a)
+}
+
+function multiply(a, b) {
+  return parseInt(a) * parseInt(b)
+}
+
+function divide(a, b) {
+  return parseInt(a) / parseInt(b)
+}
+
+function inc(n) {
+  return parseInt(n + 1)
+}
+
+function dec(n) {
+  return parseInt(n - 1)
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
